5f37ea70d9a672adba1a2b1cf88247428489f76b
Donovan An <donovan@Donovans-MacBook-Pro.local>
2017-11-27 16:21:08 -0800

    working prototype

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StoryComponent } from './story/story.component';
+import { EngagementComponent } from './engagement/engagement.component';
+import { WeddingComponent } from './wedding/wedding.component';
+import { RegistryComponent } from './registry/registry.component';
+import { RsvpComponent } from './rsvp/rsvp.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    // { path: 'home', component: HomeComponent },
+    { path: '', pathMatch: 'full', component: HomeComponent },
+    { path: 'story', component: StoryComponent },
+    { path: 'engagement', component: EngagementComponent },
+    { path: 'wedding', component: WeddingComponent },
+    { path: 'registry', component: RegistryComponent },
+    { path: 'rsvp', component: RsvpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StoryComponent } from './story/story.component';
+import { EngagementComponent } from './engagement/engagement.component';
+import { WeddingComponent } from './wedding/wedding.component';
+import { RegistryComponent } from './registry/registry.component';
+import { RsvpComponent } from './rsvp/rsvp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StoryComponent,
+    EngagementComponent,
+    WeddingComponent,
+    RegistryComponent,
+    RsvpComponent
   ],
   imports: [
     BrowserModule,
